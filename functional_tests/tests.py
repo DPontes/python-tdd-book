a1275a19212dc9e695007fcf7f2bd22459077938
DPontes <dpontes@protonmail.com>
2019-01-23 10:39:15 +0100

    attempt to fix a bug in the server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element_by_id('id_list_table')
+                table = self.browser.find_element_by_id("id_list_table")
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
